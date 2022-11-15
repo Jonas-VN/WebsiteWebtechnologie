6b867898eab4f4d6674beb9e6d5a1036e40606d2
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-11-15 15:03:04 +0100

    topnav routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sporting Lokeren' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/info', function(req, res, next) {
+  res.render('info', { title: 'Info' });
+});
+
+router.get('/tickets', function(req, res, next) {
+  res.render('tickets', { title: 'Tickets' });
+});
+
+router.get('/bussen', function(req, res, next) {
+  res.render('bussen', { title: 'Bussen' });
 });
 
 module.exports = router;
