e680592742e095975c41e6f81d19e9a4ff15821c
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-11-15 20:43:51 +0100

    change bus mdoel

--- models/bus.js
+++ models/bus.js
@@ -5,13 +5,15 @@ const Schema = mongoose.Schema;
 const BusSchema = new Schema({
   busNr: { type: Number, required: true },
   capacity: { type: Number, required: true },
+  current_amount_of_persons: { type: Number, default: 0},
   persons: { 
     type: [{ 
       type: Schema.Types.ObjectId,
       ref: "Person",
       required: true
     }],
-    validate: [arrayLimit, 'Bus exceeds maximum capacity']
+    // niet zeker ofdat dit werkt, daarom current_amount_of_persons toegevoegd zodat we ook makkelijk in de controller zouden kunnen
+    validate: [arrayLimit, 'Bus exceeds maximum capacity'] 
   }
 })
 
