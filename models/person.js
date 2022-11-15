a32e76e26e46e2c3934fdf02fc26acbdf9f2c657
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-11-15 18:47:25 +0100

    person model

---
+++ models/person.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon')
+
+const Schema = mongoose.Schema;
+
+const PersonSchema = new Schema({
+  first_name: { type: String, required: true },
+  family_name: { type: String, required: true },
+  date_of_birth: { type: Date },
+  is_adult: { type: Boolean },
+});
+
+// Format full name
+AuthorSchema.virtual("name").get(function () {
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`;
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = "";
+  }
+  return fullname;
+});
+
+// Format date of birth
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+module.exports = mongoose.model("Person", PersonSchema);
