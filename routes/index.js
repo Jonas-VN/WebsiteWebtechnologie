5101289230ddc7726d3551e1a5a267d90e5bec85
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-11-27 14:06:57 +0100

    overbodige links weg

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-router.get('/tribune1', function(req, res, next) {
-  res.render('tribune1', { title: 'Tribune 1' });
-});
-
-router.get('/tribune2', function(req, res, next) {
-  res.render('tribune2', { title: 'Tribune 2' });
-});
-
-router.get('/auto', function(req, res, next) {
-  res.render('auto', { title: 'Auto' });
-});
-
-router.get('/bus', function(req, res, next) {
-  res.render('bus', { title: 'Bus' });
-});
-
 router.get('/ticketverkoop',function(req, res, next) {
   res.render('ticketverkoop', {title: 'Ticketverkoop'})  
 });
