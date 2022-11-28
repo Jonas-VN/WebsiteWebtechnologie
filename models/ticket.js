fabbf50078b38f89c33b037771c9afaefb64e0ca
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-11-28 19:16:41 +0100

    update ticket schema

--- models/ticket.js
+++ models/ticket.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const TicketSchema = new Schema({
     persoon: { type: Schema.Types.ObjectId, ref: "Person", required: true },
+    tribune: { type: Number, required: true },
     blok: { type: String, maxLength: 1, required: true },
     rij: { type: Number, required: true },
     plaats: { type: Number, required: true}
